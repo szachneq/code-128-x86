a3bf85f151e4563ff92add2070783c4d8db0f3f2
Jan Szachno <j.szachno@gmail.com>
2023-01-14 12:30:08 +0100

    temporary wip c file

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 const int BMP_FILE_SIZE = 90122;
 
-int encode128(unsigned char *dest_bitmap, int bar_width, char *text);
+unsigned char *encode128(unsigned char *dest_bitmap, int bar_width, char *text);
 // dest_bitmap - pointer to generated image (header and pixel data)
 // bar_width - the width of the narrowest bar in pixels
 // text – text to be encoded
@@ -12,29 +12,39 @@ int encode128(unsigned char *dest_bitmap, int bar_width, char *text);
 // which can not be encoded).
 
 int main(int argc, char *argv[]) {
-    // Read the width in pixels of narrowest bar
-    int narrowest_bar_width = 1;
-    // printf("Enter the width of the narrowest bar (in pixels): ");
-    // int read_result = scanf("%d", &narrowest_bar_width);
-    // if (!read_result) return 1;
-
-    // Read the text to encode
-    char text_to_encode[40] = "33";
-    // printf("Enter the text that you want to encode: ");
-    // scanf("%40s", text_to_encode);
-
-    printf("--- \n");
-
-    printf("Narrowest bar width: %d \n", narrowest_bar_width);
-    printf("Text to encode: %s \n", text_to_encode);
-
-    printf("--- \n");
-
     unsigned char *dest_bitmap = (unsigned char *)calloc(BMP_FILE_SIZE, sizeof(unsigned char));
-    int res = encode128(dest_bitmap, narrowest_bar_width, text_to_encode);
+    int narrowest_bar_width = 1;
+    char text_to_encode[40] = "213700000000";
+    unsigned char *res = encode128(dest_bitmap, narrowest_bar_width, text_to_encode);
+    printf("Digits: \n");
     printf("%d \n", res);
+    // printf("%d \n", *(res+1));
+    // printf("%d \n", *(res+2));
 
-    free(dest_bitmap);
+
+    // // Read the width in pixels of narrowest bar
+    // int narrowest_bar_width = 1;
+    // // printf("Enter the width of the narrowest bar (in pixels): ");
+    // // int read_result = scanf("%d", &narrowest_bar_width);
+    // // if (!read_result) return 1;
+
+    // // Read the text to encode
+    // char text_to_encode[40] = "33";
+    // // printf("Enter the text that you want to encode: ");
+    // // scanf("%40s", text_to_encode);
+
+    // printf("--- \n");
+
+    // printf("Narrowest bar width: %d \n", narrowest_bar_width);
+    // printf("Text to encode: %s \n", text_to_encode);
+
+    // printf("--- \n");
+
+    // unsigned char *dest_bitmap = (unsigned char *)calloc(BMP_FILE_SIZE, sizeof(unsigned char));
+    // int res = encode128(dest_bitmap, narrowest_bar_width, text_to_encode);
+    // printf("%d \n", res);
+
+    // free(dest_bitmap);
 
 
     // Output: 
